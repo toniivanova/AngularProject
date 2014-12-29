c87f15abf765ad47a8f33623e1b2d10f53ec6501
toniivanova <toni_ivanova_@abv.bg>
2014-12-29 02:17:41 +0200

    add first page

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,3 +1,11 @@
-softUni.controller('SoftUniController', function ($scope) {
-$scope.name='name';
+softUni.controller('SoftUniController', function ($scope,mainData) {
+    mainData.getAllAds(function(resp){
+        $scope.data=resp;
+    });
+    mainData.getAllTowns(function(resp){
+        $scope.towns=resp;
+    });
+    mainData.getAllCategories(function(resp){
+        $scope.categories=resp;
+    });
 })
---
+++ app/js/services/mainPage.js
@@ -0,0 +1,31 @@
+softUni.factory('mainData', function ($http, $log) {
+    return{
+        getAllAds:function(success){
+            $http({method:'GET', url:'http://softuni-ads.azurewebsites.net/api/ads?PageSize=10&Startpage=1'})
+                .success(function(data,status,headers,config){
+                    success(data);
+                })
+                .error(function(data,status,headers,config){
+                    $log.warn(data);
+                })
+        },
+        getAllTowns: function(success) {
+            $http({method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/towns'})
+                .success(function (data, status, headers, config) {
+                    success(data);
+                })
+                .error(function (data, status, headers, config) {
+                    $log.warn(data);
+                })
+        },
+        getAllCategories: function(success) {
+            $http({method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/categories'})
+                .success(function (data, status, headers, config) {
+                    success(data);
+                })
+                .error(function (data, status, headers, config) {
+                    $log.warn(data);
+                })
+        }
+    }
+})
