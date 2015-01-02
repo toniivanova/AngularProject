45a207102c8734e4c5b47d954eed921c2048da44
toniivanova <toni_ivanova_@abv.bg>
2015-01-03 01:17:52 +0200

    add newAdv

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,5 +1,10 @@
 softUni.controller('SoftUniController', function ($scope, $location, $routeParams, mainData) {
-
+    mainData.getAllTowns(function(resp){
+        $scope.towns=resp;
+    });
+    mainData.getAllCategories(function(resp){
+        $scope.categories=resp;
+    });
 });
 
 softUni.controller('LoginController', function ($scope, $location) {
@@ -9,6 +14,42 @@ softUni.controller('LoginController', function ($scope, $location) {
 
     $scope.login= function () {
         console.log($scope.credentials.username);
-        $location.path('/newAd');
+        //$location.path('/newAd');
     };
 });
+
+softUni.controller('UserPublishNewAdController',
+    function ($scope, $rootScope, $location, townsService, categoriesService,
+              userService, notifyService) {
+        $scope.adData = {townId: null, categoryId: null};
+        $scope.categories = categoriesService.getCategories();
+        $scope.towns = townsService.getTowns();
+
+        $scope.fileSelected = function(fileInputField) {
+            delete $scope.adData.imageDataUrl;
+            var file = fileInputField.files[0];
+            if (file.type.match(/image\/.*/)) {
+                var reader = new FileReader();
+                reader.onload = function() {
+                    $scope.adData.imageDataUrl = reader.result;
+                    $(".image-box").html("<img src='" + reader.result + "'>");
+                };
+                reader.readAsDataURL(file);
+            } else {
+                $(".image-box").html("<p>File type not supported!</p>");
+            }
+        };
+
+        $scope.publishAd = function(adData) {
+            userService.createNewAd(adData,
+                function success() {
+                    notifyService.showInfo("Advertisement submitted for approval. Once approved, it will be published.");
+                    $location.path("/user/ads");
+                },
+                function error(err) {
+                    notifyService.showError("Publish ad failed", err);
+                }
+            );
+        };
+    }
+);
--- app/js/services/app.js
+++ app/js/services/app.js
@@ -15,15 +15,15 @@ var softUni=angular.module('softUniModule',['ngRoute', 'ui.bootstrap'])
             templateUrl:'templates/login.html',
             controller: 'LoginController'
         });
-        $routeProvider.when('/newAd',{
-            templateUrl:'templates/newAd.html'
-
-        });
         $routeProvider.when('/logout',{
             templateUrl:'templates/all-ads.html'
         });
         $routeProvider.when('/editProfile',{
             templateUrl:'templates/editProfile.html'
+        });
+        $routeProvider.when('/newAdv',{
+            templateUrl:'templates/newAdv.html'
+
         });
         $routeProvider.otherwise({redirectTo:'/ads'});
     });
